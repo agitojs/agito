bc36aec1975230a67e17d201d334a61bcf94213a
Aymeric Beaumet <aymeric@beaumet.me>
2014-07-27 18:06:13 +0200

    chore: add lint and test tasks to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,21 @@
 
 var gulp = require('gulp');
 var istanbul = require('gulp-istanbul');
+var jscs = require('gulp-jscs');
+var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
 
 var paths = {
-  scripts: ['lib/**/*.js'],
-  tests: ['test/**/*.js']
+  gulpfile: 'gulpfile.js',
+  scripts: 'lib/**/*.js',
+  tests: 'test/**/*.js'
 };
 
 gulp.task('coverage', function(done) {
-  gulp.src(paths.scripts)
+  gulp.src([paths.scripts])
     .pipe(istanbul())
     .on('finish', function() {
-      gulp.src(paths.tests)
+      gulp.src([paths.tests])
         .pipe(mocha())
         .pipe(istanbul.writeReports({ reporters: ['lcovonly'] }))
         .on('end', done)
@@ -22,8 +25,17 @@ gulp.task('coverage', function(done) {
   ;
 });
 
+gulp.task('lint', function() {
+  return gulp.src([paths.gulpfile, paths.scripts, paths.tests])
+    .pipe(jshint())
+    .pipe(jscs())
+  ;
+});
+
 gulp.task('test', function() {
-  return gulp.src(paths.tests, { read: false })
+  return gulp.src([paths.tests], { read: false })
     .pipe(mocha())
   ;
 });
+
+gulp.task('default', ['lint', 'test']);
