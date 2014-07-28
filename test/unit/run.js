6c9ef676da6c954848fc23e7ea0d7ec7667b0896
Aymeric Beaumet <aymeric@beaumet.me>
2014-07-28 03:05:36 +0200

    feat: can now run middlewares

--- test/unit/run.js
+++ test/unit/run.js
@@ -1,6 +1,9 @@
 'use strict';
 
-var expect = require('chai').expect;
+var chai = require('chai');
+var expect = chai.expect;
+var sinon = require('sinon');
+chai.use(require('sinon-chai'));
 
 describe('Agito#run()', function() {
 
@@ -10,6 +13,37 @@ describe('Agito#run()', function() {
    */
   it('should be a function', function() {
     expect(run).to.be.a('function');
+  });
+
+  /*
+   */
+  it('should throw if the middleware container is not an array', function() {
+    expect(function() {
+      run.call({ middlewares: {} });
+    }).to.throw();
+  });
+
+  /*
+   */
+  it('should throw if no middlewares has been registered', function() {
+    expect(function() {
+      run.call({ middlewares: [] });
+    }).to.throw();
+  });
+
+  /*
+   */
+  it('should call every registered middlewares once', function() {
+    var agito = {
+      middlewares: [1, 2, 3].map(function() {
+        return sinon.spy(function middleware(agito, done) { return done(); });
+      })
+    };
+    run.call(agito);
+
+    agito.middlewares.forEach(function(middleware) {
+      expect(middleware).to.have.been.calledOn(agito);
+    });
   });
 
 });
