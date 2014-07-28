3e068d279cfde460d79b836ba2f4063bb740d3e9
Aymeric Beaumet <aymeric@beaumet.me>
2014-07-28 02:03:16 +0200

    style: change test organization

--- test/unit/agito.js
+++ test/unit/agito.js
@@ -6,20 +6,29 @@ describe('Agito', function() {
 
   var Agito = require('../../lib/Agito');
 
+  /*
+   */
   it('should be a function', function() {
     expect(Agito).to.be.a('function');
   });
 
+  /*
+   */
   it('should create an object using the "new" operator', function() {
     var agito = new Agito();
-    expect(agito).to.be.an.instanceOf(Agito);
+
+    expect(agito).to.be.an.instanceof(Agito);
   });
 
+  /*
+   */
   it('should create an object even without the "new" operator', function() {
     var agito = Agito(); // jshint ignore:line
-    expect(agito).to.be.an.instanceOf(Agito);
+    expect(agito).to.be.an.instanceof(Agito);
   });
 
+  /*
+   */
   it('should expose the "use" method', function() {
     var agito = new Agito();
     expect(agito).to.respondTo('use');
