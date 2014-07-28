e0dedd4e417c954d19bd182ef8e070976b4c0015
Aymeric Beaumet <aymeric@beaumet.me>
2014-07-28 02:04:43 +0200

    feat: can now register middleware

--- test/unit/use.js
+++ test/unit/use.js
@@ -6,8 +6,20 @@ describe('Agito#use()', function() {
 
   var use = require('../../lib/use');
 
+  /*
+   */
   it('should be a function', function() {
     expect(use).to.be.a('function');
+  });
+
+  /*
+   */
+  it('should register a new middleware', function() {
+    var agito = {};
+    use.call(agito, function middleware() {});
+
+    expect(agito.middlewares).to.be.an.instanceof(Array);
+    expect(agito.middlewares).to.have.length(1);
   });
 
 });
