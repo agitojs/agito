39f40bcd3124869d7b1a8419d9ac3575bb90338e
Aymeric Beaumet <aymeric@beaumet.me>
2014-08-01 23:19:50 +0200

    refactor: registered middlewares are no truly private

--- test/unit/Agito.js
+++ test/unit/Agito.js
@@ -5,18 +5,14 @@ var expect = chai.expect;
 var sinon = require('sinon');
 chai.use(require('sinon-chai'));
 
-var AgitoMock = require('./mocks/Agito');
-
-/**
- * Create a spy middleware.
- */
-function createMiddleware() {
-  return sinon.spy(function middleware(agito, done) { return done(); });
-}
-
 describe('Agito', function() {
 
   var Agito = require('../../lib/Agito');
+  var agito;
+
+  beforeEach(function() {
+    agito = new Agito();
+  });
 
   /*
    */
@@ -27,42 +23,26 @@ describe('Agito', function() {
   /*
    */
   it('should create an object using the "new" operator', function() {
-    var agito = new Agito();
+    expect(agito).to.be.an.instanceOf(Agito);
+  });
+
+  /*
+   */
+  it('should create an object even without using the "new" operator', function() { // jshint ignore:line
+    agito = Agito(); // jshint ignore:line
 
     expect(agito).to.be.an.instanceOf(Agito);
   });
 
   /*
    */
-  it('should create an object even without the "new" operator', function() {
-    var agito = Agito(); // jshint ignore:line
-
-    expect(agito).to.be.an.instanceOf(Agito);
-  });
-
-  /*
-   */
-  it('should expose a middleware container (privately)', function() {
-    var agito = new Agito();
-
-    expect(agito).to.have.property('_middlewares');
-    expect(agito._middlewares).to.be.an.instanceOf(Array);
-    expect(agito._middlewares).to.be.empty; // jshint ignore:line
-  });
-
-  /*
-   */
-  it('should expose a protocol container (publicly)', function() {
-    var agito = new Agito();
-
+  it('should expose a protocol container', function() {
     expect(agito).to.have.property('protocols');
     expect(agito.protocols).to.be.an.instanceOf(Array);
     expect(agito.protocols).to.be.empty; // jshint ignore:line
   });
 
-  it('should expose a redirection container (publicly)', function() {
-    var agito = new Agito();
-
+  it('should expose a redirection container', function() {
     expect(agito).to.have.property('redirections');
     expect(agito.redirections).to.be.an.instanceOf(Array);
     expect(agito.redirections).to.be.empty; // jshint ignore:line
@@ -71,130 +51,72 @@ describe('Agito', function() {
   /*
    */
   it('should expose the "use" method', function() {
-    var agito = new Agito();
-
     expect(agito).to.respondTo('use');
   });
 
   /*
    */
   it('should expose the "run" method', function() {
-    var agito = new Agito();
-
     expect(agito).to.respondTo('run');
   });
 
   describe('#use()', function() {
 
-    var agito;
-    var use = Agito.prototype.use;
-
-    beforeEach(function() {
-      agito = AgitoMock.create();
-    });
-
     /*
      */
     it('should be a function', function() {
-      expect(use).to.be.a('function');
-    });
-
-    /*
-     */
-    it('should register a new middleware', function() {
-      var middleware = function() {};
-      use.call(agito, middleware);
-
-      expect(agito._middlewares).to.be.an.instanceOf(Array);
-      expect(agito._middlewares).to.have.length(1);
-      expect(agito._middlewares).to.include(middleware);
-    });
-
-    /*
-     */
-    it('should support several consecutive calls', function() {
-      var ret = use.call(
-        use.call(agito, function middlewareA() {}),
-        function middlewareB() {}
-      );
-
-      expect(ret).to.deep.equal(agito);
-      expect(ret._middlewares).to.have.length(2);
-    });
-
-    /*
-     */
-    it('should throw if the middleware attribute is not an array or undefined', function() { // jshint ignore:line
-      agito._middlewares = null;
-
-      expect(function() {
-        agito.use();
-      }).to.throw();
+      expect(agito.use).to.be.a('function');
     });
 
   });
 
   describe('#run()', function() {
 
-    var agito;
-    var run = Agito.prototype.run;
-
-    beforeEach(function() {
-      agito = AgitoMock.create();
-    });
-
     /*
      */
     it('should be a function', function() {
-      expect(run).to.be.a('function');
-    });
-
-    /*
-     */
-    it('should throw if the middleware container is not an array', function() {
-      agito._middlewares = null;
-
-      expect(function() {
-        run.call(agito);
-      }).to.throw();
+      expect(agito.run).to.be.a('function');
     });
 
     /*
      */
     it('should throw if no middlewares have been registered', function() {
       expect(function() {
-        run.call(agito);
+        agito.run();
       }).to.throw();
     });
 
     /*
      */
     it('should call every registered middleware once', function() {
-      agito._middlewares = [1, 2, 3].map(function() { return createMiddleware(); });
-      run.call(agito);
+      var middleware = sinon.spy(function(agito, done) { return done(); });
+      agito
+        .use(middleware)
+        .run()
+      ;
 
-      agito._middlewares.forEach(function(middleware) {
-        expect(middleware).to.have.been.calledOn(agito);
-        expect(middleware).to.have.been.calledWith(agito);
-      });
+      expect(middleware).to.have.been.calledOn(agito);
+      expect(middleware).to.have.been.calledWith(agito);
     });
 
     /*
      */
     it('should return null to avoid accidental chaining', function() {
-      agito._middlewares = [createMiddleware()];
-      var ret = run.call(agito);
+      var ret = agito
+        .use(function(agito, done) { return done(); })
+        .run()
+      ;
 
-      expect(ret).to.equal(null);
+      expect(ret).to.be.null; // jshint ignore:line
     });
 
     /*
      */
     it('should throw if one middleware returns an error', function() {
-      agito._middlewares.push(function(agito, done) { return done('Error'); });
+      agito.use(function(agito, done) { return done('Error'); });
 
       expect(function() {
-        run.call(agito);
+        agito.run();
       }).to.throw();
     });
 
