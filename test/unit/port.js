08a2829bb1492c578affc3db6cbc02e8c5b6a55c
Aymeric Beaumet <aymeric@beaumet.me>
2014-08-02 12:36:30 +0200

    feat: add intelligence to detect default ports

---
+++ test/unit/port.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('port', function() {
+
+  var port = require('../../lib/port');
+
+  it('should expose a `getDefaultPortForProtocol` method', function() {
+    expect(port).to.respondTo('getDefaultPortForProtocol');
+  });
+
+  describe('#getDefaultPortForProtocol()', function() {
+
+    var getDefaultPortForProtocol = port.getDefaultPortForProtocol;
+
+    it('should return null if the given port doesn\'t exist', function() {
+      var ret = getDefaultPortForProtocol('unknown');
+
+      expect(ret).to.be.null; // jshint ignore:line
+    });
+
+    it('should return a number if the port exists in the map', function() {
+      var ret = getDefaultPortForProtocol('http');
+
+      expect(ret).to.equal(80);
+    });
+
+  });
+
+});
