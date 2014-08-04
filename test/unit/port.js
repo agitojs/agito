ae4bba249beb909cfcf8754b0383af5a13cf073a
Aymeric Beaumet <aymeric@beaumet.me>
2014-08-04 20:37:44 +0200

    refactor: aggregate the listeners by port

--- test/unit/port.js
+++ test/unit/port.js
@@ -8,10 +8,6 @@ describe('port', function() {
     expect(port).to.respondTo('getDefaultPortForProtocol');
   });
 
-  it('should expose a `extractPorts` method', function() {
-    expect(port).to.respondTo('extractPorts');
-  });
-
   describe('#getDefaultPortForProtocol()', function() {
 
     var getDefaultPortForProtocol = port.getDefaultPortForProtocol;
@@ -26,58 +22,6 @@ describe('port', function() {
       var ret = getDefaultPortForProtocol('http');
 
       expect(ret).to.equal(80);
-    });
-
-  });
-
-  describe('#extractPorts()', function() {
-
-    var extractPorts = port.extractPorts;
-
-    it('should return an array', function() {
-      var ret = extractPorts([]);
-
-      expect(ret).to.be.an.instanceOf(Array);
-    });
-
-    it('should return all the ports', function() {
-      var triggers = [
-        { pattern: { port: 80 } },
-        { pattern: { port: 8080 } }
-      ];
-      var ret = extractPorts(triggers);
-
-      expect(ret).to.deep.equal([80, 8080]);
-    });
-
-    it('should aggregate ports to avoid duplicate', function() {
-      var triggers = [
-        { pattern: { port: 80 } },
-        { pattern: { port: 80 } }
-      ];
-      var ret = extractPorts(triggers);
-
-      expect(ret).to.deep.equal([80]);
-    });
-
-    it('should throw an error if a pattern is not an object', function() {
-      var triggers = [
-        { pattern: undefined }
-      ];
-
-      expect(function() {
-        extractPorts(triggers);
-      }).to.throw(/^trigger.pattern should be an object: /);
-    });
-
-    it('should throw an error if a port is not a number', function() {
-      var triggers = [
-        { pattern: { port: undefined } }
-      ];
-
-      expect(function() {
-        extractPorts(triggers);
-      }).to.throw(Error, /^trigger.pattern.port should be a number: /);
     });
 
   });
