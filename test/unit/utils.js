cb2ae20a0e9269e8b6aa0b9ab3a153b990be07b8
Aymeric Beaumet <aymeric@beaumet.me>
2014-08-04 23:19:21 +0200

    refactor: group triggers by port, hostname and protocol

---
+++ test/unit/utils.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var getEven = function(n) {
+  return (n % 2) === 0 ? 'even' : 'odd';
+};
+
+var getSign = function(n) {
+  if (n === 0) { return 'null'; }
+  return (n > 0) ? 'positive' : 'negative';
+};
+
+describe('utils', function() {
+
+  var utils = require('../../lib/utils');
+
+  it('should expose a `nestedGroupBy` method', function() {
+    expect(utils).to.respondTo('nestedGroupBy');
+  });
+
+  describe('#nestedGroupBy()', function() {
+
+    var nestedGroupBy = utils.nestedGroupBy;
+
+    it('should return the collection untouched if no identities are provided', function() {
+      var collection = [1, 2, 3];
+
+      expect(nestedGroupBy(collection)).to.deep.equal(collection);
+      expect(nestedGroupBy(collection, null)).to.deep.equal(collection);
+      expect(nestedGroupBy(collection, [])).to.deep.equal(collection);
+    });
+
+    it('should group by the given identity', function() {
+      var collection = [0, 1, 2, 3, 4, 5];
+      var ret = nestedGroupBy(collection, getEven);
+
+      expect(ret).to.deep.equal({
+        even: [0, 2, 4],
+        odd:  [1, 3, 5]
+      });
+    });
+
+    it('should permit nested grouping', function() {
+      var collection = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
+      var ret = nestedGroupBy(collection, [getEven, getSign]);
+
+      expect(ret).to.deep.equal({
+        even: {
+          negative: [-4, -2],
+          null: [0],
+          positive: [2, 4]
+        },
+        odd: {
+          negative: [-5, -3, -1],
+          positive: [1, 3, 5]
+        }
+      });
+    });
+
+    it('should allow grouping by key', function() {
+      var collection = [
+        { foo: '10' },
+        { foo: '20' },
+        { foo: '10' },
+        { foo: '20' }
+      ];
+      var ret = nestedGroupBy(collection, 'foo');
+
+      expect(ret).to.deep.equal({
+        10: [{ foo: '10' }, { foo: '10' }],
+        20: [{ foo: '20' }, { foo: '20' }]
+      });
+    });
+
+  });
+
+});
